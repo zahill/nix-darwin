9e55eda2d510bd23d7b383a36eb1b33d3348d661
zhill2 <zachary.a.hill@outlook.com>
2024-11-03 10:37:11 -0800

    Added dotfiles

--- home.nix
+++ home.nix
@@ -4,6 +4,15 @@
   home.username = "zach";
   home.homeDirectory = "/Users/zach";
 
+  home.files = {
+    ".zshrc".source = ./dotfiles/.zshrc;
+    ".config/nvim".source = ./dotfiles/nvim;
+    ".config/kitty".source = ./dotfiles/kitty;
+    ".config/aerospace".source = ./dotfiles/aerospace;
+    ".config/neofetch".source = ./dotfiles/neofetch;
+    ".config/raycast".source = ./dotfiles/raycast;
+  };
+
   home.stateVersion = "24.05";
 
   programs.home-manager.enable = true;
